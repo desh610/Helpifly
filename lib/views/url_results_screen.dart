98a05bf4fe54f18cdf16a2e0f9a7130f32037583
Deshan <deshan.biss@gmail.com>
2024-08-05 11:36:55 +0530

    URL Screen Update

--- lib/views/url_results_screen.dart
+++ lib/views/url_results_screen.dart
@@ -7,7 +7,6 @@ import 'package:helpifly/constants/colors.dart';
 import 'package:helpifly/helper/helper_functions.dart';
 import 'package:helpifly/widgets/custom_textfield.dart';
 import 'package:helpifly/widgets/custom_button.dart';
-import 'package:helpifly/widgets/skeletons.dart';
 import 'package:helpifly/widgets/widgets_exporter.dart';
 import 'package:liquid_progress_indicator/liquid_progress_indicator.dart';
 
@@ -19,72 +18,71 @@ class UrlResultsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: true,
       backgroundColor: primaryColor,
-      body: Padding(
-        padding: EdgeInsets.only(left: 15, right: 15, top: 40),
+      body: GestureDetector(
+        onTap: () {
+          closeKeyboard(context); // Close the keyboard when tapping outside
+        },
         child: SingleChildScrollView(
           physics: BouncingScrollPhysics(),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                "Check product quality\nby URL",
-                style: TextStyle(
-                  color: white,
-                  fontSize: 32,
-                  fontWeight: FontWeight.bold,
+          child: Padding(
+            padding: EdgeInsets.only(left: 15, right: 15, top: 40),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Check product quality\nby URL",
+                  style: TextStyle(
+                    color: white,
+                    fontSize: 32,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
-              SizedBox(height: 0),
-              CustomTextField(
-                controller: _urlController,
-                hintText: "Enter product URL here",
-                overlineText: "Product URL",
-                minLines: 5,
-                maxLines: 5,
-                buttonText: "Clear",
-                onTapTextButton: () {
-                  _urlController.clear();
-                  context.read<UrlResultsCubit>().reset();
-                },
-              ),
-              SizedBox(height: 15),
-              BlocBuilder<UrlResultsCubit, UrlResultsState>(
-                builder: (context, state) {
-                  if (state.isLoading) {
-                    return Text("Please wait...", style: TextStyle(color: white),);
-                  } 
-                  else {
+                SizedBox(height: 20),
+                CustomTextField(
+                  controller: _urlController,
+                  hintText: "Enter product URL here",
+                  overlineText: "Product URL",
+                  minLines: 5,
+                  maxLines: 5,
+                  buttonText: "Clear",
+                  onTapTextButton: () {
+                    _urlController.clear();
+                    context.read<UrlResultsCubit>().reset();
+                  },
+                ),
+                SizedBox(height: 15),
+                BlocBuilder<UrlResultsCubit, UrlResultsState>(
+                  builder: (context, state) {
                     return Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         Text(
                           "Results",
                           style: TextStyle(
-                              fontSize: 18,
-                              color: white,
-                              fontWeight: FontWeight.bold),
+                            fontSize: 18,
+                            color: white,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
                         SizedBox(height: 5),
                         CustomPercentage(
                           title: "Positive",
-                          percentage:
-                              "${state.positivePercentage.toStringAsFixed(2)}%",
-                          fillPercentage: state.positivePercentage,
+                          percentage: state.isLoading ? "..." : "${state.positivePercentage.toStringAsFixed(2)}%",
+                          fillPercentage: state.isLoading ? 0 : state.positivePercentage,
                           fillColor: green,
                         ),
                         CustomPercentage(
                           title: "Negative",
-                          percentage:
-                              "${state.negativePercentage.toStringAsFixed(2)}%",
-                          fillPercentage: state.negativePercentage,
+                          percentage: state.isLoading ? "..." : "${state.negativePercentage.toStringAsFixed(2)}%",
+                          fillPercentage: state.isLoading ? 0 : state.negativePercentage,
                           fillColor: red,
                         ),
                         CustomPercentage(
                           title: "Neutral",
-                          percentage:
-                              "${state.neutralPercentage.toStringAsFixed(2)}%",
-                          fillPercentage: state.neutralPercentage,
+                          percentage: state.isLoading ? "..." : "${state.neutralPercentage.toStringAsFixed(2)}%",
+                          fillPercentage: state.isLoading ? 0 : state.neutralPercentage,
                           fillColor: CupertinoColors.activeOrange,
                         ),
                         SizedBox(height: 20),
@@ -94,26 +92,28 @@ class UrlResultsScreen extends StatelessWidget {
                             Text(
                               "Overall Product Quality",
                               style: TextStyle(
-                                  fontSize: 18,
-                                  color: white,
-                                  fontWeight: FontWeight.bold),
+                                fontSize: 18,
+                                color: white,
+                                fontWeight: FontWeight.bold,
+                              ),
                             ),
                             Container(
                               height: 90,
                               width: 90,
                               child: LiquidCircularProgressIndicator(
-                                value: state.positivePercentage / 100,
+                                value: state.isLoading ? 0 : state.positivePercentage / 100,
                                 valueColor: AlwaysStoppedAnimation(green),
                                 backgroundColor: Colors.transparent,
                                 borderColor: white.withOpacity(0.5),
                                 borderWidth: 2,
                                 direction: Axis.vertical,
                                 center: Text(
-                                  "${state.positivePercentage.toStringAsFixed(2)}%",
+                                  state.isLoading ? "..." : "${state.positivePercentage.toStringAsFixed(2)}%",
                                   style: TextStyle(
-                                      fontSize: 18,
-                                      color: white,
-                                      fontWeight: FontWeight.w500),
+                                    fontSize: 18,
+                                    color: white,
+                                    fontWeight: FontWeight.w500,
+                                  ),
                                 ),
                               ),
                             ),
@@ -121,33 +121,29 @@ class UrlResultsScreen extends StatelessWidget {
                         ),
                       ],
                     );
-                  }
-                },
-              ),
-              //  SizedBox(height: MediaQuery.of(context).size.height * 0.06),
-              BlocBuilder<UrlResultsCubit, UrlResultsState>(
-                builder: (context, state) {
-                  return Padding(
-                    padding: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.09),
-                    child:  
-                    ValueListenableBuilder(
-                          valueListenable: _urlController,
-                          builder: (context, _, __) {
-                            return CustomButton(
-                              onTap: () {
-                                closeKeyboard(context);
-                                context.read<UrlResultsCubit>().analyzeUrl(_urlController.text, context);
-                              },
-                              buttonText: "Check Quality",
-                              enabled: _urlController.text.isNotEmpty && !state.isLoading,
-                            );
+                  },
+                ),
+                SizedBox(height: MediaQuery.of(context).size.height * 0.07),
+                BlocBuilder<UrlResultsCubit, UrlResultsState>(
+                  builder: (context, state) {
+                    return ValueListenableBuilder(
+                      valueListenable: _urlController,
+                      builder: (context, _, __) {
+                        return CustomButton(
+                          onTap: () {
+                            closeKeyboard(context);
+                            context.read<UrlResultsCubit>().analyzeUrl(_urlController.text, context);
                           },
-                        ),
-                  );
-                },
-              ),
-              SizedBox(height: 15,)
-            ],
+                          buttonText: "Check Quality",
+                          enabled: _urlController.text.isNotEmpty && !state.isLoading,
+                        );
+                      },
+                    );
+                  },
+                ),
+                SizedBox(height: 15),
+              ],
+            ),
           ),
         ),
       ),
